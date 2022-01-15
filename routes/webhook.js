88521048beb2d0857d33239aa901333acccaae05
PatrykFij <patryk.fijalkowski1@users.noreply.github.com>
2022-01-15 20:55:08 +0100

    added webhook endpoint

---
+++ routes/webhook.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.post("/", function (req, res, next) {
+  console.log("Visited webhook");
+  console.log(req.body);
+  res.send("Shoper webhoook");
+});
+
+module.exports = router;
