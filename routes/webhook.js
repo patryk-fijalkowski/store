bb7603a274bd36b261c8efdc95b4ad519b409d09
PatrykFij <patryk.fijalkowski1@users.noreply.github.com>
2022-01-16 20:58:58 +0100

    Connected with firestore

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,10 +1,21 @@
 var express = require("express");
+var User = require("../db");
 var router = express.Router();
-
+var db = require("../db");
 /* GET users listing. */
 router.post("/", function (req, res, next) {
   console.log("Visited webhook");
-  console.log(req.body);
+
+  const data = {
+    date: req.body.date,
+    sum: req.body.sum,
+    source: req.body.payment.name === "Allegro" ? "Allegro" : "Sklep",
+    products: JSON.stringify(req.body.products),
+    body: JSON.stringify(req.body),
+  };
+  db.collection("Orders").doc().set(data);
+  console.log("saved");
+
   res.send("Shoper webhoook");
 });
 
